cd2d12bf698f704f0aebf5a3531db5caca5aae53
Gustavo <gustavo@devnx.com.br>
2022-03-18 14:44:07 -0300

    att

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
                                             <Link className={css.link} href="/maquinas">
                                                 <a className={css.linkmenu}> PRODUTOS </a>
                                             </Link>
-                                            <Image className={css.image} src="/down.svg" alt="logo" width={20} height={20} />
+                                            <Image src="/down.svg" alt="logo" width={19} height={19} />
                                         </button>
                                         <div className={css.dropdowncontent}>
                                             <div>
@@ -39,10 +39,14 @@ export default function Navbar() {
                                                                 Caterpillar
                                                             </a>
                                                         </Link>
-                                                        <Image className={css.image} src="/down2.svg" alt="logo" width={18} height={18} />
+                                                        <Image className={css.image} src="/arrowright.svg" alt="logo" width={15} height={15} />
                                                     </button>
-                                                    <div className={css.dropdowncontent2}>
+                                                    <div style={{float: 'right'}}>
+                                                    <div style={{width: 150,}} className={css.dropdowncontent2}>
                                                         <div>
+                                                            <div style={{marginLeft: 5,}} className="p-2 pb-0 fs-5">
+                                                                Caterpillar
+                                                            </div>
                                                             <Link className={css.nomemaq} href="/maquinas/caterpillar/pth18">
                                                                 <a>
                                                                     PTH 18
@@ -58,15 +62,19 @@ export default function Navbar() {
                                                             </Link>
                                                         </div>
                                                     </div>
+                                                    </div>
                                                 </div>
                                                 <div className={css.dropdown2}>
                                                     <button style={{ margin: 0, marginBottom: 15, }} className={css.dropbtn2}>
                                                         <Link className={css.link} href="/maquinas"> LogSet </Link>
-                                                        <Image className={css.image} src="/down2.svg" alt="logo" width={18} height={18} />
+                                                        <Image className={css.image} src="/arrowright.svg" alt="logo" width={15} height={15} />
                                                     </button>
+                                                    <div style={{float: 'right'}}>
                                                     <div className={css.dropdowncontent3}>
-                                                        <Row>
-                                                            <Col>
+                                                        <div style={{marginLeft: 5,}} className="p-2 pb-0 fs-5">
+                                                            LogSet
+                                                        </div>
+                                                        <div className="d-flex align-items-center">
                                                                 <Link className={css.nomemaq} href="/maquinas/logset/4fgt">
                                                                     <a>4F GT</a>
                                                                 </Link>
@@ -79,9 +87,8 @@ export default function Navbar() {
                                                                 <Link className={css.nomemaq} href="/maquinas/logset/5hpgt">
                                                                     <a>5HP GT</a>
                                                                 </Link>
-
-                                                            </Col>
-                                                            <Col>
+                                                      </div>
+                                                        <div className="d-flex align-items-center">
                                                                 <Link className={css.nomemaq} href="/maquinas/logset/6fgt">
                                                                     <a>6F GT</a>
                                                                 </Link>
@@ -94,36 +101,42 @@ export default function Navbar() {
                                                                 <Link className={css.nomemaq} href="/maquinas/logset/8fgt">
                                                                     <a>8F GT</a>
                                                                 </Link>
-                                                            </Col>
-                                                            <Col>
+                                                        </div>
+                                                        <div className="d-flex align-items-center">
+                                                            <Link className={css.nomemaq} href="/maquinas/logset/8hgt">
+                                                                <a>8H GT</a>
+                                                            </Link>
                                                                 <Link className={css.nomemaq} href="/maquinas/logset/8hblack">
                                                                     <a>8H GTE BLACK</a>
                                                                 </Link>
-                                                                <Link className={css.nomemaq} href="/maquinas/logset/8hgt">
-                                                                    <a>8H GT</a>
-                                                                </Link>
-                                                                <Link className={css.nomemaq} href="/maquinas/logset/8hgthybrid">
-                                                                    <a>8H GTE Hybrid</a>
-                                                                </Link>
-                                                                <Link className={css.nomemaq} href="/maquinas/logset/10fgt">
-                                                                    <a>10F GT</a>
-                                                                </Link>
-                                                                <Link className={css.nomemaq} href="/maquinas/logset/12hybrid">
-                                                                    <a>12H GTE Hybrid</a>
-                                                                </Link>
-                                                            </Col>
-                                                        </Row>
+                                                            <Link className={css.nomemaq} href="/maquinas/logset/8hgthybrid">
+                                                                <a>8H GTE Hybrid</a>
+                                                            </Link>
+
+                                                        </div>
+                                                        <div className="d-flex align-items-center">
+                                                            <Link className={css.nomemaq} href="/maquinas/logset/10fgt">
+                                                                <a>10F GT</a>
+                                                            </Link>
+                                                            <Link className={css.nomemaq} href="/maquinas/logset/12hybrid">
+                                                                <a>12H GTE Hybrid</a>
+                                                            </Link>
+
+                                                        </div>
                                                     </div>
+                                                </div>
                                                 </div>
                                                 <div className={css.dropdown2}>
                                                     <button style={{ marginTop: -20, }} className={css.dropbtn2}>
                                                         <Link className={css.link} href="/maquinas"> Weiler </Link>
-                                                        <Image className={css.image} src="/down2.svg" alt="logo" width={18} height={18} />
+                                                        <Image className={css.image} src="/arrowright.svg" alt="logo" width={15} height={15} />
                                                     </button>
-                                                    <div className={css.dropdowncontent2}>
-                                                        <Row>
-                                                            <Col>
-                                                                <div>
+                                                    <div style={{float: 'right'}}>
+                                                    <div style={{width: 270,}} className={css.dropdowncontent2}>
+                                                        <div style={{marginLeft: 5,}} className="p-2 pb-0 fs-5">
+                                                            Weiler
+                                                        </div>
+                                                        <div className="d-flex align-items-center">
                                                                     <Link className={css.nomemaq} href="/maquinas/weiler/b457">
                                                                         <a>B 457</a>
                                                                     </Link>
@@ -137,8 +150,7 @@ export default function Navbar() {
                                                                         <a>H 458</a>
                                                                     </Link>
                                                                 </div>
-                                                            </Col>
-                                                            <Col>
+                                                        <div className="d-flex align-items-center">
                                                                 <Link className={css.nomemaq} href="/maquinas/weiler/s250">
                                                                     <a>S 250</a>
                                                                 </Link>
@@ -151,8 +163,8 @@ export default function Navbar() {
                                                                 <Link className={css.nomemaq} href="/maquinas/weiler/s550">
                                                                     <a>S 550</a>
                                                                 </Link>
-                                                            </Col>
-                                                        </Row>
+                                                        </div>
+                                                    </div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -174,11 +186,6 @@ export default function Navbar() {
                             </Col>
                         </Row>
 
-                    </div>
-                    <div className={css.flex}>
-                        <div className={css.borda}>
-
-                        </div>
                     </div>
                 </Container>
             </div>
