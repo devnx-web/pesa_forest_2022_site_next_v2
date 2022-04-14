083f5cefe013725ba005fa6ef2aa8e9e65851c48
fabiodevn1 <40078286+fabiodevn1@users.noreply.github.com>
2022-04-14 18:31:05 -0300

    Update headermob.js

--- pages/components/headermob.js
+++ pages/components/headermob.js
@@ -41,13 +41,12 @@ export default function Headermob() {
                         </Link>
                     </li>
                     <div className={css.botao}>
-                                <Link href="https://api.whatsapp.com/send?phone=554121032211&text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20PESA%20Forest%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es!"className={css.linkbotao}>
-                                <a target="_blank"  className={css.botaoctt}>
-                                <Image  src="/whatsapp.png" alt="logo" width={15} height={15}/>
-                                ‏‏‎ ‎‏‏‎ ‎(41) 2103-2211
-                                </a>
-                                </Link>
-                                </div>
+                        <Link target="_blank"  href="https://api.whatsapp.com/send?phone=554121032211&text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20PESA%20Forest%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es!"className={css.linkbotao}>
+                            <a className={css.botaoctt}>
+                            <Image  src="/whatsapp.png" alt="logo" width={15} height={15}/> (41) 2103-2211
+                            </a>
+                        </Link>
+                    </div>
                 </ul>
                 
                 <div className={css.alignlogo}>
