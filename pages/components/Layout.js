5deac1c61a912800fc572a3c24f71e1bb3208a6d
Gabriel Cardoso <68553837+GabrielCardoso-psd@users.noreply.github.com>
2022-05-23 10:16:13 -0300

    Update Layout.js

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,10 +1,13 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Head from 'next/head'
+import { useState } from 'react'
+import Script from 'next/script'
 
 export default function Layout({ children }) {
     return (
         <>
+        <Script id="GTM-WCK92HL"/>
             <Head>
                 <title>Pesa Forest</title>
             </Head>
